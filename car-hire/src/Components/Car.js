6e16bccb770cab7d9103bfa8392b52be2966b075
Alice <alice.bailey@ecs.co.uk>
2021-10-20 14:07:18 +0100

    credit checker

--- car-hire/src/Components/Car.js
+++ car-hire/src/Components/Car.js
@@ -5,12 +5,43 @@ import BuyCar from "./BuyCar";
 class Car extends Component {
   state = {
     car: {},
+    wantToBuy: false,
+    creditScore: 0,
   };
 
   componentDidMount = () => {
     getCar(this.props.car_id).then((car) => {
       this.setState({ car: car });
     });
+  };
+
+  onClick = () => {
+    if (this.state.wantToBuy === false) {
+      this.setState({ wantToBuy: true });
+    } else {
+      this.setState({ wantToBuy: false });
+    }
+  };
+
+  creditCheck = (event) => {
+    event.preventDefault();
+
+    let randomScore = Math.floor(Math.random() * 700);
+    if (this.state.creditScore > 700) {
+      alert("Your credit score can't be more than 700 silly");
+    } else if (this.state.creditScore < randomScore) {
+      alert(
+        `Your credit score must be more that ${randomScore} to buy this car`
+      );
+    } else {
+      alert(
+        "congratulation your credit score is high enough to purchase this car"
+      );
+    }
+  };
+
+  handleChange = (data) => {
+    this.setState({ creditScore: data });
   };
 
   render() {
@@ -24,7 +55,26 @@ class Car extends Component {
         <p>
           {description}, {cost}, {colour}
         </p>
-        <BuyCar car_id={this.props.car_id} />
+        <button onClick={this.onClick}>Find out more!</button>
+        {this.state.wantToBuy ? (
+          <div>
+            <p>
+              So you want to buy this car, first lets check your credit score{" "}
+            </p>
+            <form onSubmit={this.creditCheck}>
+              <input
+                type="text"
+                placeholder="Credit score*"
+                name="name"
+                value={this.state.creditScore}
+                onChange={(event) => this.handleChange(event.target.value)}
+              ></input>
+              <button>Submit Credit Score</button>
+            </form>
+            <button>Buy Car</button>
+          </div>
+        ) : null}
+        {/* <BuyCar car_id={this.props.car_id} /> */}
       </div>
     );
   }
